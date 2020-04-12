9d5867be34e735bc3582ebc1354b25e8dca572ad
Blake Hawkins <hawkinsba@hendrix.edu>
2020-04-11 21:06:56 -0500

    Create temperature_func.c
    Exercise 1-15. Rewrite the temperature conversion program to use a function for conversion.

---
+++ Chapter 1/temperature_func.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define LOWER   0
+#define UPPER   300
+#define STEP    20
+
+float fahr_to_cel(int f);
+
+int main() {
+    for (int f = LOWER; f <= UPPER; f = f + STEP) {
+        printf("%3d %6.1f\n", f, fahr_to_cel(f));
+    }
+}
+
+float fahr_to_cel(int f) {
+    return (5.0 / 9.0) * (f - 32);
+}
